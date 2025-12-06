6566d863008522c27787bcf63762dd537efb5164
Martin Bruzina <martin@bruzina.cz>
2025-12-06 13:12:51 +0100

    style: fix clang-format detected issues

--- tests/test_unit.cpp
+++ tests/test_unit.cpp
@@ -1,22 +1,24 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include <cstdlib>
 #include <array>
+#include <cstdlib>
 #include <memory>
 #include <string>
 
 std::string run_app() {
-    std::array<char,128> buffer{};
-    std::string result;
+  std::array<char, 128> buffer{};
+  std::string result;
 
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen("./cpp-cli", "r"), pclose);
-    if (!pipe) return {};
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen("./cpp-cli", "r"),
+                                                pclose);
+  if (!pipe)
+    return {};
 
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
-        result += buffer.data();
-    return result;
+  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
+    result += buffer.data();
+  return result;
 }
 
 TEST_CASE("Program prints correct output") {
-    REQUIRE(run_app() == "Hello world\n");
+  REQUIRE(run_app() == "Hello world\n");
 }
